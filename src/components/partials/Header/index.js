9dd6dee00be99afca80326a4d89bac2e48dafdb1
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-07-26 16:32:49 -0300

    Header>Logo

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+
 import { HeaderArea } from "./styled";
 
 const Header = () => {
-  return <HeaderArea>...</HeaderArea>;
+  return (
+    <HeaderArea>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="logo-1">S</span>
+            <span className="logo-2">Y</span>
+            <span className="logo-3">S</span>
+          </Link>
+        </div>
+      </div>
+    </HeaderArea>
+  );
 };
 
 export default Header;
