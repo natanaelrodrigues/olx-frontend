ccf3aef906e936786ad3a943e65872d77b1fc0bd
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-07-26 17:30:01 -0300

    Inclusão do 404 na rota

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import About from './pages/About';
-import Home from './pages/Home';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-export default () => {
-    return(
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/about">
-                <About />
-            </Route>
-        </Switch>
-    );
-}
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
